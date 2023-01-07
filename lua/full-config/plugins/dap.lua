2d5806727f9be31ca31cebd7d7584b2b8414a75e
domas-v <domas.vaitmonas93@gmail.com>
2023-01-07 14:45:23 +0200

    Prettify the file and add python configuration

--- lua/full-config/plugins/dap.lua
+++ lua/full-config/plugins/dap.lua
@@ -1,47 +1,39 @@
 local dap = require('dap')
 local dapui = require("dapui")
 
+--- dapui
 dapui.setup()
 require("nvim-dap-virtual-text").setup({
     virt_text_win_col = 30
 })
 
-require('dap-python').setup("python")
-
-
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+    dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 
+--- python
+require('dap-python').setup("python")
+require('dap-python').test_runner = 'pytest'
+table.insert(require('dap').configurations.python, {
+    type = 'python',
+    request = 'launch',
+    name = 'tms',
+    program = '../venv/bin/chalice',
+    args =  { "local", "--no-autoreload" },
+    cwd = "/Users/domev/Work/tenspeed-lambda/tms"
+})
 
+--- 
 local sign = vim.fn.sign_define
-
 sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
 sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
 sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
-
-
--- vim.fn.sign_define('DapStopped', {
---     text='▶', texthl='WarningMsg',linehl='DapUIBreakpointsCurrentLine', numhl='ModeMsg'})
---
--- vim.fn.sign_define('DapBreakpoint', {
---     text='●', texthl='ErrorMsg', linehl='', numhl=''})
---
--- vim.fn.sign_define('DapBreakpointCondition', {
---     text='⊕', texthl='ErrorMsg', linehl='', numhl=''})
---
--- vim.fn.sign_define('DapLogPoint', {
---     text='', texthl='ErrorMsg', linehl='', numhl=''})
---
--- vim.fn.sign_define('DapBreakpointRejected', {
---     text='⨷', texthl='ErrorMsg', linehl='', numhl=''})
-
 
 vim.keymap.set("n", "<leader>du", "<cmd>lua require('dapui').toggle()<cr>")
 vim.keymap.set("n", "<leader>dr", "<cmd>lua require('dap').repl.toggle()<cr>")
@@ -49,6 +41,7 @@ vim.keymap.set("n", "<leader>dt", "<cmd>DapVirtualTextToggle<cr>")
 vim.keymap.set("n", "<leader>de", "<cmd>lua require('dapui').eval()<cr>")
 vim.keymap.set("n", "<leader>df", "<cmd>lua require('dapui').float_element()<cr>")
 vim.keymap.set("n", "<leader>dR", "<cmd>DapVirtualTextForceRefresh<cr>")
+vim.keymap.set("n", '<leader>dm', '<cmd>lua require("dap-python").test_method()<cr>')
 vim.keymap.set("n", "<F5>", '<cmd>lua require"dap".continue()<cr>')
 vim.keymap.set("n", "<F6>", '<cmd>lua require"dap".terminate()<cr>')
 vim.keymap.set("n", "<F8>", '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<cr>')
